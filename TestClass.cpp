90ba390420ab1261615615441db11cf042e53a3b
jp Nie <1921863583@qq.com>
2016-05-16 14:24:21 +0800

    Add files via upload

---
+++ TestClass.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Animal
+{
+public:
+	Animal()
+	{
+
+	}
+	~Animal()
+	{
+
+	} 
+	void eat()
+	{
+
+	}
+	void eat(int x, int y)
+	{
+
+	}
+protected:
+
+};
+
+
+class Fish :public Animal
+{
+public:
+
+};
